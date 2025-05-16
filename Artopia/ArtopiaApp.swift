315469fb4aced86de677f0630b7f4477d074b9f4
Alex Fila <fialex66@gmail.com>
2025-05-16 09:28:02 +0200

    Initial Commit

---
+++ Artopia/ArtopiaApp.swift
@@ -0,0 +1,13 @@
+// ArtopiaApp.swift
+// (c) Alex Fila 16/05/25 
+
+import SwiftUI
+
+@main
+struct ArtopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Artopia/ContentView.swift
@@ -0,0 +1,20 @@
+// ContentView.swift
+// (c) Alex Fila 16/05/25 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
