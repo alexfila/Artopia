d140363c648b36d10871421032e5bdebb0b181e6
Alex Fila <fialex66@gmail.com>
2025-06-03 09:15:09 +0200

    adding add values view

--- Artopia/AddView.swift
+++ Artopia/AddView.swift
@@ -4,8 +4,24 @@
 import SwiftUI
 
 struct AddView: View {
+    @State private var hemoglobin: String = ""
+    @State private var neutrophils: String = ""
+    @State private var platelets: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Hemoglobin (e.g. 10.0)", text: $hemoglobin)
+                    .keyboardType(.decimalPad)
+                
+                TextField("Neutrophils (e.g. 10.0)", text: $neutrophils)
+                    .keyboardType(.decimalPad)
+                
+                TextField("Platelets (e.g. 10.0)", text: $platelets)
+                    .keyboardType(.decimalPad)
+            }
+        }
+        .navigationTitle("Add Values")
     }
 }
 
--- Artopia/CustomButtons.swift
+++ Artopia/CustomButtons.swift
@@ -3,20 +3,6 @@
 
 import SwiftUI
 
-struct CustomButtons: View {
-//    var symbol: String
-//    var text: String
-    
-//    init(_ button: String) { switch button {
-//    case "Upload": symbol = "chevron.backward.circle", text = "Upload"
-//    case "Scan": symbol = "chevron.forward.circle", text = "Scan"
-//    case "Add", _: symbol = "checkmark.circle.fill", text = "Add"}}
-    
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
 struct BloodButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
@@ -27,6 +13,3 @@ struct BloodButtonStyle: ButtonStyle {
             .cornerRadius(15)
     }
 }
-
-
-#Preview { CustomButtons() }
